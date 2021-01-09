42414d21e09011f006ef8dc626e1b0beaa034949
Ryan Fitzsimmons <ryan.fitzsimmons@outlook.com>
2021-01-09 19:02:45 +0000

    Changed stage status indices

--- AutomationFramework/StageStatuses.cs
+++ AutomationFramework/StageStatuses.cs
@@ -1,18 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace AutomationFramework
 {
     [Serializable]
     public enum StageStatuses
     {
+        Errored = -3,
+        Cancelled = -2,
+        Disabled = -1,
         None = 0,
         Bypassed = 1,
         Running = 2,
         Completed = 3,
-        Cancelled = 4,
-        Errored = 5,
-        Disabled = 6,
     }
 }
