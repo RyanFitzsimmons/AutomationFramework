d9486040ec1f7851182f6f12925455a779c14f95
Ryan Fitzsimmons <ryan.fitzsimmons@outlook.com>
2021-01-09 19:21:48 +0000

    Added JsonSerializerExtensions

---
+++ AutomationFramework/JsonSerializerExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json;
+
+namespace AutomationFramework
+{
+    public static class JsonSerializerExtensions
+    {
+        public static T FromJson<T>(this string input, JsonSerializerOptions options = default) where T : class =>
+            JsonSerializer.Deserialize<T>(input, options);
+
+        public static string ToJson<T>(this T input, JsonSerializerOptions options = default) where T : class =>
+            JsonSerializer.Serialize<T>(input, options);
+
+        public static T ToAnonymousType<T>(string json, T anonymousTypeObject, JsonSerializerOptions options = default)
+            => JsonSerializer.Deserialize<T>(json, options);
+    }
+}
