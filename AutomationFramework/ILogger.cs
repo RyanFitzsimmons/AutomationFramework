0c9dd5648b7c7e0bbc5b2b983b7b3e79782bc64c
Ryan Fitzsimmons <ryan.fitzsimmons@outlook.com>
2021-01-19 23:13:24 +0000

    Added IDisposable to ILogger

--- AutomationFramework/ILogger.cs
+++ AutomationFramework/ILogger.cs
@@ -1,6 +1,8 @@
-﻿namespace AutomationFramework
+﻿using System;
+
+namespace AutomationFramework
 {
-    public interface ILogger
+    public interface ILogger : IDisposable
     {
         void Write(LogLevels level, object message);
         void Write(LogLevels level, StagePath path, object message);        
